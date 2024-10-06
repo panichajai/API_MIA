835d7c8714b58af36f50b3e2ca006a22a269fb29
Mona Lisa <panichaboomjail262@gmail.com>
2024-10-07 02:19:46 +0700

    [EDIT] App.js and package

--- express-api/app.js
+++ express-api/app.js
@@ -35,7 +35,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/', indexRouter);
+app.use('/api', indexRouter);
 app.use('/api/users', userRoutes);
 app.use('/api/products', products);
 app.use('/api/customers', customersRouter);
@@ -50,6 +50,9 @@ app.use(function(err, req, res, next) {
 
   res.status(err.status || 500);
   res.render('error');
+});
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 module.exports = app;
