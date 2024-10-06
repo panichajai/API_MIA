d5733d6850229ad7eb7eb8c55dc43909b1f9ca71
Mona Lisa <panichaboomjail262@gmail.com>
2024-10-07 02:35:06 +0700

    [EDIT] app.js

--- express-api/app.js
+++ express-api/app.js
@@ -35,7 +35,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api', indexRouter);
+app.use('/', indexRouter);
 app.use('/api/users', userRoutes);
 app.use('/api/products', products);
 app.use('/api/customers', customersRouter);
